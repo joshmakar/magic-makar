d872e746925aa03dc423f992eb2760238f4f8bd3
joshmakar <joshmakar@gmail.com>
2019-09-02 16:27:34 -0400

    General update

--- functions-mm.php
+++ functions-mm.php
@@ -1,5 +1,6 @@
 <?php
 
+// Enqueue Scripts
 function magic_makar_enqueue_styles_scripts() {
   wp_enqueue_style('google-fonts', '//fonts.googleapis.com/css?family=Raleway:300,400,700,900');
   wp_enqueue_script('font-awesome', 'https://kit.fontawesome.com/00aef0d8f3.js', '', '', true);
@@ -7,5 +8,9 @@ function magic_makar_enqueue_styles_scripts() {
 }
 
 add_action('wp_enqueue_scripts', 'magic_makar_enqueue_styles_scripts');
+
+
+// Set Post Thumbnail Size
+set_post_thumbnail_size(811, 456, true);
 
 ?>
